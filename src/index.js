1b7e95ec43b472aa145b603e8713544f3fb71988
yousefbahutair <yousefnas06@gmail.com>
2025-03-23 10:27:30 +0400

    feat: add logic to onTick for circle border movement

--- src/index.js
+++ src/index.js
@@ -1,39 +1,25 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton){
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-        this.startButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
-    }
-    start = () => {
-        this.interval = setInterval(() => {
-         this.tick()
-        }, 1000);
-    };
-
-    pause = () => {
-      clearInterval(this.interval)
-    };
-    onDurationChange(){};
-    tick(){
-      if(this.timeRemaining <= 0){
-        this.pause;
-        alert("END OF THE TIMER");
-      } else{
-        this.timeRemaining = this.timeRemaining - 1;
-      }
-    };
-    get timeRemaining(){
-        return parseInt(durationInput.value);
-    }
-    set timeRemaining(time) {
-        durationInput.value = time;
-    }
-}
-
 const durationInput = document.querySelector('#duration');
-const startButton = document.querySelector('#start');
-const pauseButton = document.querySelector('#pause');
+const startButton   = document.querySelector('#start');
+const pauseButton   = document.querySelector('#pause');
+const circleBorder  = document.querySelector('#circle')
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circleBorder.setAttribute('stroke-dasharray',perimeter);
+
+let duration;
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart(totalDuration) {
+        duration = totalDuration;
+        console.log("TIMER STARTED");
+    },
+    onTick(timeRemaining) {
+        circleBorder.setAttribute('stroke-dashoffset',
+        perimeter * timeRemaining / duration - perimeter
+        );
+        console.log("TICKED");
+
+    },
+    onComplete() {
+        console.log("COMPLETED!");
+    }
+});
